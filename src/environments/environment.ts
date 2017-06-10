c53b6c3589e964429193488551eb85b868340ad5
Igor-Kwin <sinevor13@gmail.com>
2017-06-10 15:10:50 +0300

    firebase conection

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyC_FBnCt23N_38KIPFZqeE6IjEuVm1nM80',
+    authDomain: 'noteapp-85b8c.firebaseapp.com',
+    databaseURL: 'https://noteapp-85b8c.firebaseio.com',
+    projectId: 'noteapp-85b8c',
+    storageBucket: 'noteapp-85b8c.appspot.com',
+    messagingSenderId: '248576039623'
+  }
 };
